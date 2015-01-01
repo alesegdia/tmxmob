bc76c47afee2c7b874885b7cd8f94f82e275b1f9
Alejandro Seguí <alesegdia@gmail.com>
2015-01-01 21:37:34 +0100

    Project started.

---
+++ project/html/src/com/ufofrog/tmxmob/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.ufofrog.tmxmob.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.ufofrog.tmxmob.TmxMobApp;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new TmxMobApp();
+        }
+}
